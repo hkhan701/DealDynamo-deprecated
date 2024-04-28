b06de017f53705add3e019d404164873c2846573
hkhan701 <96705217+hkhan701@users.noreply.github.com>
2024-04-28 17:49:20 -0500

    Remove unused configuration files and code

--- tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-config = configparser.ConfigParser()
-config.read('configuration.ini')
-default = config['DEFAULT']
-print(default['SECRET_KEY'])
-app.secret_key = default['SECRET_KEY']
-app.config['MONGO_DBNAME'] = default['MONGO_DBNAME'] # os.environ.get('MONGO_DBNAME')
-app.config['MONGO_URI'] = default['MONGODB_URI'] #default['MONGO_URI']
-print(app.config['MONGO_URI'])
-app.config['PREFERRED_URL_SCHEME'] = "https"
