a664c330c809a98fb26b971912813c85304e762e
hkhan701 <96705217+hkhan701@users.noreply.github.com>
2024-04-28 21:44:42 -0500

    Update APP_LOCATION check in run.py to use 'vercel' instead of 'heroku'

--- run.py
+++ run.py
@@ -369,7 +369,7 @@ def is_safe_url(target):
 
 
 # Heroku environment
-if os.environ.get('APP_LOCATION') == 'heroku':
+if os.environ.get('APP_LOCATION') == 'vercel':
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
 else:
